6e9616d65d7aeef34dc0b7651c5914e836887a3b
Matt Gerton <matt.gerton@gmail.com>
2013-04-10 18:28:24 -0500

    Refactoring library. Going with a more straightforward architecture.

--- src/guier.js
+++ src/guier.js
@@ -4,15 +4,83 @@
  * @version 0.1
  */
 ;(function(window, undefined) {
+	// var Guier = window.Guier || {};
+	// window.Guier = Guier;
+
+	var VERSION = '0.1.0';
+
+	/**
+	 * Create the Guier object to attach methods to
+	 */
 	var Guier = window.Guier || {};
+
+	/**
+	 * @constructor
+	 * Bootstraps the library by storing references to the Google 
+	 * Maps API objects
+	 */
+	Guier.init = function init(args) {
+		console.info('Initializing Guier...');
+
+		// An object literal where the parsed geocode data will be stored
+		this.geoObject = {};
+
+		// References to the Google API objects
+		this.geocoder = (args.geocoder) ? args.geocoder : undefined;
+		this.coordinates = (args.latlng) ? args.latlng : undefined;
+
+		// We need these objects in order to make the magic happen
+		if (this.geocoder === undefined || this.coordinates === undefined) {
+			throw new Error('Guier requires Google Maps API objects to function.');
+		}
+	};
+
+	/**
+	 * @private
+	 * Makes the API call to the Geocoder service
+	 *
+	 * @returns {Object} gp
+	 * 			The Geoposition object
+	 */
+	Guier._reverseGeocode = function reverseGeocode() {
+		this.geocoder.geocode({ 'latlng': this.coordinates }, _parseGeocodeObject);
+	};
+
+	/**
+	 * @private
+	 * Callback function used when Google geocode() method is called. This 
+	 * method parses the returned Geocoder object and sets it to an internal 
+	 * object that is used to return data via the public API methods.
+	 * 
+	 * @param {Object} results
+	 * 			An object with the resulting geocode data
+	 * @param {String} status
+	 * 			A status code
+	 */
+	Guier._parseGeocodeObject = function parseGeocodeObject(results, status) {
+		if (status === google.maps.GeocoderStatus.OK) {
+			// return the results
+			console.log('Geocoder parsing happens here.');
+		}
+	};
+
+	/**
+	 * Main accessor method. Takes a string representing the requested 
+	 * geocode data and an optional callback and returns the information
+	 * 
+	 * @param {String} attr
+	 * 			A string containing the type of requested data
+	 * @param {Function} callback
+	 * 			An optional callback function
+	 */
+	Guier.get = function get(attr, callback) {
+		//
+	};
+
+	// Expose the Guier object for use
 	window.Guier = Guier;
 
 	Guier = (function() {
-		// Private methods and properties
-		var VERSION = '0.1.0';
-		var _geocoder;
-		var _coordsObj;
-		
 		/**
 		 * Makes the API call to the Geocoder service
 		 * 
